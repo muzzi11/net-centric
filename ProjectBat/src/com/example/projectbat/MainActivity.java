f5e44152158b9296193a86ac4d8337c0fa423d4d
Mussi Kara <muzzi11@hotmail.com>
2013-06-11 13:47:06 +0200

    Created the mic recording class.

--- ProjectBat/src/com/example/projectbat/MainActivity.java
+++ ProjectBat/src/com/example/projectbat/MainActivity.java
@@ -4,12 +4,17 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
-
+public class MainActivity extends Activity
+{
+	Player player;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        player = new Player(this);
+        player.play();
     }
 
 
@@ -18,6 +23,5 @@ public class MainActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
-    }
-    
+    }   
 }
