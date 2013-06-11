b868bb20952a34cc45de350eb0fa735ece607474
JBonsink <josbonsink@gmail.com>
2013-06-11 14:17:02 +0200

    Bluetooth

--- ProjectBat/gen/com/example/projectbat/R.java
+++ ProjectBat/gen/com/example/projectbat/R.java
@@ -24,10 +24,18 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080006;
+        public static final int button_scan=0x7f080005;
+        public static final int goBlue=0x7f080000;
+        public static final int new_devices=0x7f080004;
+        public static final int paired_devices=0x7f080002;
+        public static final int title_new_devices=0x7f080003;
+        public static final int title_paired_devices=0x7f080001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int device_list=0x7f030001;
+        public static final int device_name=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f070000;
--- ProjectBat/src/com/example/projectbat/MainActivity.java
+++ ProjectBat/src/com/example/projectbat/MainActivity.java
@@ -1,23 +1,39 @@
 package com.example.projectbat;
 
+import com.example.projectbat.R;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class MainActivity extends Activity {
-
+public class MainActivity extends Activity 
+{	
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) 
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-
+        
+        // Initialize the button to perform device discovery
+        Button scanButton = (Button) findViewById(R.id.goBlue);
+        scanButton.setOnClickListener(new OnClickListener() 
+        {       	
+            public void onClick(View v) 
+            {
+            	Intent intent = new Intent(MainActivity.this, DeviceListActivity.class);
+            	startActivity(intent);
+            }
+        });      
+    }    
+        
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-    
 }
