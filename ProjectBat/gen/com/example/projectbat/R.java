9bf648eb3d2e374fed5b158356c697174f835bc0
JBonsink <josbonsink@gmail.com>
2013-06-19 10:04:58 +0200

    Removed the old broken bluetooth implementation.

--- ProjectBat/gen/com/example/projectbat/R.java
+++ ProjectBat/gen/com/example/projectbat/R.java
@@ -35,24 +35,18 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f09000a;
+        public static final int action_settings=0x7f090005;
         public static final int beep=0x7f090004;
         public static final int connectionList=0x7f090000;
         public static final int goBlue=0x7f090003;
         public static final int macAddress=0x7f090001;
-        public static final int new_devices=0x7f090008;
-        public static final int paired_devices=0x7f090006;
         public static final int plot=0x7f090002;
-        public static final int textView1=0x7f090009;
-        public static final int title_new_devices=0x7f090007;
-        public static final int title_paired_devices=0x7f090005;
     }
     public static final class layout {
         public static final int activity_bluetooth=0x7f030000;
         public static final int activity_histogram=0x7f030001;
         public static final int activity_main=0x7f030002;
-        public static final int device_list=0x7f030003;
-        public static final int device_name=0x7f030004;
+        public static final int device_name=0x7f030003;
     }
     public static final class menu {
         public static final int bluetooth=0x7f080000;
