556b99c52b62c9f6824d4adb735f6f739fd10ac8
JBonsink <josbonsink@gmail.com>
2013-06-19 15:13:28 +0200

    Added a textview which displays received messages from other devices via BT.

--- ProjectBat/gen/com/example/projectbat/R.java
+++ ProjectBat/gen/com/example/projectbat/R.java
@@ -35,12 +35,13 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f090005;
-        public static final int beep=0x7f090004;
-        public static final int connectionList=0x7f090000;
-        public static final int goBlue=0x7f090003;
-        public static final int macAddress=0x7f090001;
-        public static final int plot=0x7f090002;
+        public static final int action_settings=0x7f090006;
+        public static final int beep=0x7f090005;
+        public static final int connectionList=0x7f090001;
+        public static final int goBlue=0x7f090004;
+        public static final int macAddress=0x7f090000;
+        public static final int plot=0x7f090003;
+        public static final int receivedMsg=0x7f090002;
     }
     public static final class layout {
         public static final int activity_bluetooth=0x7f030000;
